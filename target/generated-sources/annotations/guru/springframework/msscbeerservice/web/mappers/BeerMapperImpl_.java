0e5897426fe856b2adeb18d2dd15130804214229
Bennett <Aleks.Bennett@bregroup.com>
2020-05-11 13:24:14 +0100

    Added in Autowiring. It fails the CI build and Maven packaging but works when running.

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-05-11T12:40:25+0100",
+    date = "2020-05-11T13:20:00+0100",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.6 (AdoptOpenJDK)"
 )
 @Component
