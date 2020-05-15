8f2a7278e1bca8bba9e1d9e23963b337da1fa458
Bennett <Aleks.Bennett@bregroup.com>
2020-05-15 10:43:48 +0100

    Updated POM to use public BOM. Added embedded DB setup for h2. Added mysql init script.

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-05-14T11:46:12+0100",
+    date = "2020-05-15T09:42:28+0100",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.6 (AdoptOpenJDK)"
 )
 @Component
